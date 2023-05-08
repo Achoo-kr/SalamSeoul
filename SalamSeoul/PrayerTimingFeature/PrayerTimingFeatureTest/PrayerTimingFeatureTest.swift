35e91a71b81cf0dd447a3df225ad9e2e7ee88f8d
Choo Hyun Ho <hyunho4178@gmail.com>
2023-05-09 03:08:06 +0900

    add test files
    add test files

---
+++ SalamSeoul/PrayerTimingFeature/PrayerTimingFeatureTest/PrayerTimingFeatureTest.swift
@@ -0,0 +1,54 @@
+//
+//  PrayerTimingFeatureTest.swift
+//  PrayerTimingFeatureTest
+//
+//  Created by 추현호 on 2023/05/09.
+//
+
+import XCTest
+import ComposableArchitecture
+import FirebaseAuth
+@testable import SalamSeoul
+
+final class RegistrationFeatureTest: XCTestCase {
+    
+    //로그인, 로그아웃, 회원가입, 회원탈퇴 테스트
+    
+    
+//    func testSignIn() {
+//        let store = TestStore(
+//            initialState: RegistrationState(),
+//            reducer: registrationReducer,
+//            environment: .init(
+//                client: .live,
+//                mainQueue: .main.eraseToAnyScheduler())
+//        )
+//
+//        let dummyUser = User(
+//            email: "11@11.com",
+//            password: "111",
+//            userName: "111",
+//            isPro: false
+//        )
+//
+//
+//        store.send(.signUpButtonTapped(username: "111", email: "11@11.com", password: "111")) {
+//                $0.email = "11@11.com"
+//                $0.userName = "111"
+//            }
+//
+//        store.receive(.signUpResponse(.success(authDataResult))) {
+//                $0.alertText = "Successfully signed up!"
+//            }
+//
+////            .do { self.testScheduler.advance(by: .seconds(1)) },
+////            .receive(.loginResponse(.success(AuthDataResult()))) {
+////                $0.uid = ""
+////                $0.isSignedIn = true
+////                $0.alertText = "Successfully signed in!"
+////            }
+//
+//
+//    }
+    
+}
---
+++ SalamSeoul/RestaurantMainFeature/RestaurantMainFeatureTest/RestaurantMainFeatureTest.swift
@@ -0,0 +1,51 @@
+//
+//  RestaurantMainFeatureTest.swift
+//  RestaurantMainFeatureTest
+//
+//  Created by 추현호 on 2023/05/09.
+//
+
+import XCTest
+import ComposableArchitecture
+@testable import SalamSeoul
+
+final class RestaurantMainFeatureTest: XCTestCase {
+    //https://www.pointfree.co/episodes/ep195-async-composable-architecture-the-problem
+    
+    //비동기적으로 코드 수정 요망
+    
+//    func testObserveSuccess() {
+//        let store = TestStore(
+//            initialState: RestaurantState(),
+//            reducer: restaurantReducer,
+//            environment: .init(
+//                client: .mock,
+//                mainQueue: .main.eraseToAnyScheduler())
+//        )
+//
+//        let testRestaurants: [Restaurant] = [
+//            Restaurant(address: "Seoul",
+//                       category: "Turkish food",
+//                       name: "SalamSeoul"),
+//            Restaurant(address: "Seoul",
+//                       category: "Turkish food",
+//                       name: "Choo Kebab"),
+//            Restaurant(address: "Seoul",
+//                       category: "Turkish food",
+//                       name: "Hyunho Kebab"),
+//        ]
+//
+//        let scheduler = DispatchQueue.testScheduler
+//
+//        store.assert(
+//            .send(.startObserve),
+//            .do {
+//                // Advance the scheduler by 1 second to allow the effect to complete
+//                scheduler.advance(by: 5)
+//            },
+//            .receive(.updateRestaurants(.success(testRestaurants))) {
+//                XCTAssertEqual($0.restaurants, testRestaurants)
+//            }
+//        )
+//    }
+}
